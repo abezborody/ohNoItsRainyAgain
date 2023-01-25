74315b3dbbaeaec9b214b3da4733ce358646fad1
Anton Bezborody <kukanujke@gmail.com>
2023-01-25 16:07:56 +0400

    Fetch current weather data, render current weather component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import Search from "./components/Search/Search.component";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather.component";
 
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./utils/api";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
+
   const onSearchChangeHandler = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastWeatherFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastWeatherFetch])
+      .then(async (res) => {
+        const weatherResponse = await res[0].json();
+        const forecastResponse = await res[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
   };
+
+  currentWeather && console.log(currentWeather);
+  // console.log(forecastWeather);
+
   return (
     <div className="container">
       <Search onSearchChange={onSearchChangeHandler} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/components/CurrentWeather/CurrentWeather.component.jsx
+++ src/components/CurrentWeather/CurrentWeather.component.jsx
@@ -1,36 +1,50 @@
 import "./CurrentWeather.css";
 
-const CurrentWeather = (city) => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="currentWeather-container">
       <div className="currentWeather-top">
         <div>
-          <p className="currentWeather-mainInfo-city">Belgrade</p>
-          <p className="currentWeather-mainInfo-description">Rainy</p>
+          <p className="currentWeather-mainInfo-city">{data.city}</p>
+          <p className="currentWeather-mainInfo-description">
+            {data.weather[0].description}
+          </p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
       </div>
       <div className="currentWeather-bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="details-row border">
             <span className="detail-label">Details</span>
           </div>
           <div className="details-row">
             <span className="detail-label">Feels like</span>
-            <span className="detail-value">25°C</span>
+            <span className="detail-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className="details-row">
             <span className="detail-label">Wind</span>
-            <span className="detail-value">5 m/s</span>
+            <span className="detail-value">
+              {Math.round(data.wind.speed)} m/s
+            </span>
           </div>
           <div className="details-row">
             <span className="detail-label">Humidity</span>
-            <span className="detail-value">20%</span>
+            <span className="detail-value">
+              {Math.round(data.main.humidity)}%
+            </span>
           </div>
           <div className="details-row">
             <span className="detail-label">Pressure</span>
-            <span className="detail-value">722 mm</span>
+            <span className="detail-value">
+              {Math.round(data.main.pressure)} mm
+            </span>
           </div>
         </div>
       </div>
