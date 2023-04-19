c82e4839c86eb517af15b3d50d82b555287dfdaa
Anton Bezborody <59689681+abezborody@users.noreply.github.com>
2023-04-19 17:32:28 +0400

    changed colors, fix accordion

--- src/components/Forecast/Forecast.component.jsx
+++ src/components/Forecast/Forecast.component.jsx
@@ -6,7 +6,8 @@ import {
   AccordionItemPanel,
 } from 'react-accessible-accordion';
 import './Forecast.styles.css';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 const WEEK_DAYS = [
   'Monday',
@@ -32,72 +33,76 @@ const Forecast = ({ data }) => {
         transition={{ delay: 0.2, duration: 0.5 }}
         className='forecast-title'
       >
-        Daily
+        Daily forecast
       </motion.label>
       <Accordion allowZeroExpanded className='accordion-container'>
-        {data.list.splice(0, 7).map((item, index) => (
-          <motion.div
-            animate={{ y: 0, opacity: 1 }}
-            initial={{ y: 16, opacity: 0 }}
-            transition={{
-              type: 'spring',
-              delay: 0.2 + index * 0.05,
-              stiffness: 150,
-            }}
-          >
-            <AccordionItem key={index}>
-              <AccordionItemHeading>
-                <AccordionItemButton>
-                  <div className='daily-item '>
-                    <img
-                      alt='daily weather icon'
-                      className='icon-small'
-                      src={`icons/${item.weather[0].icon}.png`}
-                    />
-                    <label className='daily-item-day'>
-                      {forecastDays[index]}
-                    </label>
-                    <label className='daily-item-description'>
-                      {item.weather[0].description}
-                    </label>
-                    <label className='daily-item-min-max'>
-                      {Math.round(item.main.temp_min)}°C /
-                      {Math.round(item.main.temp_max)}°C
-                    </label>
+        <AnimatePresence>
+          {data.list.splice(0, 7).map((item, index) => (
+            <motion.div
+              key={index}
+              exit={{ scale: 0.5, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              initial={{ y: 16, opacity: 0 }}
+              transition={{
+                type: 'spring',
+                delay: 0.2 + index * 0.05,
+                stiffness: 150,
+              }}
+            >
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton onChange={}>
+                    <div className='daily-item '>
+                      <img
+                        alt='daily weather icon'
+                        className='icon-small'
+                        src={`icons/${item.weather[0].icon}.png`}
+                      />
+                      <label className='daily-item-day'>
+                        {forecastDays[index]}
+                      </label>
+                      <label className='daily-item-description'>
+                        {item.weather[0].description}
+                      </label>
+                      <label className='daily-item-min-max'>
+                        {Math.round(item.main.temp_min)}°C /
+                        {Math.round(item.main.temp_max)}°C
+                      </label>
+                    </div>
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <div className='daily-details-container'>
+                    <div className='daily-details-item'>
+                      <label>Feels like</label>
+                      <label>{Math.round(item.main.feels_like)}°C</label>
+                    </div>
+                    <div className='daily-details-item'>
+                      <label>Pressure</label>
+                      <label>{item.main.pressure} mm</label>
+                    </div>
+                    <div className='daily-details-item'>
+                      <label>Humidity</label>
+                      <label>{item.main.humidity}%</label>
+                    </div>
+                    <div className='daily-details-item'>
+                      <label>Clouds</label>
+                      <label>{item.clouds.all}%</label>
+                    </div>
+                    <div className='daily-details-item'>
+                      <label>Wind speed</label>
+                      <label>{item.wind.speed} m/s</label>
+                    </div>
+                    <div className='daily-details-item'>
+                      <label>Sea level</label>
+                      <label>{item.main.sea_level} m</label>
+                    </div>
                   </div>
-                </AccordionItemButton>
-              </AccordionItemHeading>
-              <AccordionItemPanel>
-                <div className='daily-details-container'>
-                  <div className='daily-details-item'>
-                    <label>Feels like</label>
-                    <label>{Math.round(item.main.feels_like)}°C</label>
-                  </div>
-                  <div className='daily-details-item'>
-                    <label>Pressure</label>
-                    <label>{item.main.pressure} mm</label>
-                  </div>
-                  <div className='daily-details-item'>
-                    <label>Humidity</label>
-                    <label>{item.main.humidity}%</label>
-                  </div>
-                  <div className='daily-details-item'>
-                    <label>Clouds</label>
-                    <label>{item.clouds.all}%</label>
-                  </div>
-                  <div className='daily-details-item'>
-                    <label>Wind speed</label>
-                    <label>{item.wind.speed} m/s</label>
-                  </div>
-                  <div className='daily-details-item'>
-                    <label>Sea level</label>
-                    <label>{item.main.sea_level} m</label>
-                  </div>
-                </div>
-              </AccordionItemPanel>
-            </AccordionItem>
-          </motion.div>
-        ))}
+                </AccordionItemPanel>
+              </AccordionItem>
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </Accordion>
     </>
   );
